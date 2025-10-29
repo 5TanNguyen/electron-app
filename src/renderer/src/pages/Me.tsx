abad2c8f34dc481d8a087494860ac8f28271c46d
5TanNguyen <5tannguyenhuu@gmail.com>
2025-10-29 08:58:31 +0700

    a

--- src/renderer/src/pages/Me.tsx
+++ src/renderer/src/pages/Me.tsx
@@ -1,6 +1,6 @@
 import { MdEdit } from 'react-icons/md'
 import { AiFillSignal } from 'react-icons/ai'
-import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
+import { IoIosArrowDown, IoIosArrowUp, IoIosPersonAdd } from 'react-icons/io'
 import * as React from 'react'
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
 
@@ -13,6 +13,114 @@ import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 function Me() {
   const [isOpenModal, setIsOpenModal] = React.useState(false)
+
+  const friends = [
+    {
+      id: 1,
+      name: 'Nguyễn Minh Anh',
+      img: 'https://randomuser.me/api/portraits/men/32.jpg'
+    },
+    {
+      id: 2,
+      name: 'Trần Thị Bảo Ngọc',
+      img: 'https://randomuser.me/api/portraits/women/44.jpg'
+    },
+    {
+      id: 3,
+      name: 'Phạm Hữu Tài',
+      img: 'https://randomuser.me/api/portraits/men/68.jpg'
+    },
+    {
+      id: 4,
+      name: 'Lê Thu Hà',
+      img: 'https://randomuser.me/api/portraits/women/25.jpg'
+    },
+    {
+      id: 5,
+      name: 'Đỗ Văn Duy',
+      img: 'https://randomuser.me/api/portraits/men/56.jpg'
+    },
+    {
+      id: 6,
+      name: 'Võ Thị Kim Yến',
+      img: 'https://randomuser.me/api/portraits/women/12.jpg'
+    },
+    {
+      id: 7,
+      name: 'Ngô Đức Hoàng',
+      img: 'https://randomuser.me/api/portraits/men/75.jpg'
+    },
+    {
+      id: 8,
+      name: 'Phan Mỹ Linh',
+      img: 'https://randomuser.me/api/portraits/women/41.jpg'
+    },
+    {
+      id: 9,
+      name: 'Lý Hữu Phước',
+      img: 'https://randomuser.me/api/portraits/men/24.jpg'
+    },
+    {
+      id: 10,
+      name: 'Đinh Bảo Trâm',
+      img: 'https://randomuser.me/api/portraits/women/9.jpg'
+    },
+    {
+      id: 11,
+      name: 'Nguyễn Quang Huy',
+      img: 'https://randomuser.me/api/portraits/men/80.jpg'
+    },
+    {
+      id: 12,
+      name: 'Tạ Thị Hồng Nhung',
+      img: 'https://randomuser.me/api/portraits/women/33.jpg'
+    },
+    {
+      id: 13,
+      name: 'Phùng Văn Khánh',
+      img: 'https://randomuser.me/api/portraits/men/14.jpg'
+    },
+    {
+      id: 14,
+      name: 'Hoàng Thị Minh Tú',
+      img: 'https://randomuser.me/api/portraits/women/50.jpg'
+    },
+    {
+      id: 15,
+      name: 'Trịnh Quốc Thái',
+      img: 'https://randomuser.me/api/portraits/men/91.jpg'
+    },
+    {
+      id: 13,
+      name: 'Phùng Văn Khánh',
+      img: 'https://randomuser.me/api/portraits/men/14.jpg'
+    },
+    {
+      id: 14,
+      name: 'Hoàng Thị Minh Tú',
+      img: 'https://randomuser.me/api/portraits/women/50.jpg'
+    },
+    {
+      id: 15,
+      name: 'Trịnh Quốc Thái',
+      img: 'https://randomuser.me/api/portraits/men/91.jpg'
+    },
+    {
+      id: 13,
+      name: 'Phùng Văn Khánh',
+      img: 'https://randomuser.me/api/portraits/men/14.jpg'
+    },
+    {
+      id: 14,
+      name: 'Hoàng Thị Minh Tú',
+      img: 'https://randomuser.me/api/portraits/women/50.jpg'
+    },
+    {
+      id: 15,
+      name: 'Trịnh Quốc Thái',
+      img: 'https://randomuser.me/api/portraits/men/91.jpg'
+    }
+  ]
 
   return (
     <div className="w-full h-[calc(100vh-120px)] flex justify-center">
@@ -106,32 +214,69 @@ function Me() {
         </div>
 
         <div
-          className={`mt-[160px] lg:mt-[0px] h-[150px] bg-blue-300 w-full transition-all duration-500 overflow-hidden ${isOpenModal ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+          className={`mt-[170px] lg:mt-[10px] h-[350px] w-full transition-all duration-500 overflow-hidden ${isOpenModal ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
         >
-          <div className="w-full h-[150px]">
-            <Swiper
-              modules={[Navigation, Pagination, Scrollbar, A11y]}
-              spaceBetween={50}
-              slidesPerView={3}
-              navigation
-              pagination={{ clickable: true }}
-              scrollbar={{ draggable: true }}
-              observer={true}
-              observeParents={true}
-            >
-              <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              ...
-            </Swiper>
-          </div>
+          <label htmlFor="" className="text-white font-bold text-lg mb-2">
+            Những người bạn có thể biết
+          </label>
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={8}
+            slidesPerView={7}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            observer={true}
+            observeParents={true}
+            className="h-[290px] mt-2"
+          >
+            {friends.map((p) => (
+              <SwiperSlide
+                key={p.id}
+                className="!w-[180px] h-[350px] border-2 rounded-xl border-gray-500"
+              >
+                <img src={p.img} className="w-[100%] h-[150px] rounded-t-xl" />
+                <div className="flex flex-col gap-2">
+                  <span className="text-white font-bold mt-1 ms-3">{p.name}</span>
+                  <span className="text-sm text-white mt-1 ms-3">X bạn chung</span>
+                  <button
+                    type="button"
+                    className="w-[calc(100%-20px)] mx-auto mt-1 bg-blue-400 h-[30px] rounded-3xl flex flex-row items-center justify-center gap-1"
+                  >
+                    <IoIosPersonAdd />
+                    Thêm bạn bè
+                  </button>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
         <div
-          className={`h-[150px] mt-2 bg-red-300 w-full transition-all duration-500 overflow-hidden max-h-96 opacity-100`}
+          className={`
+            flex
+            flex-col lg:flex-row
+            h-[150px] mt-2
+            bg-red-300 w-full 
+            transition-all 
+            duration-500 
+            overflow-hidden 
+            max-h-96 opacity-100
+
+            `}
         >
-          <div className="w-full"></div>
+          <div
+            className="
+            w-[100%] lg:w-[40%] 
+            h-[300px] lg:h-[200px]
+            bg-amber-500"
+          ></div>
+          <div
+            className="
+              w-[100%] lg:w-[60%] 
+              h-[300px] lg:h-[200px]
+            bg-gray-500"
+          ></div>
         </div>
       </div>
     </div>
